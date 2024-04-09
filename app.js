bd342894769cc95d9db40877e5faa6d777dd2b04
Amir Heidari <amir.heidari.khoram@gmail.com>
2024-04-09 21:34:51 +0330

    feat: server

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const port = 5000;
+
+const SUBS_FOLDER = `subs`
+
+app.get('/subs/:remark', (req, res) => {
+    const id = req.header('Authorization');
+    if (!id) {
+        return res.status(401).send('Unauthorized');
+    }
+
+    const { remark } = req.params;
+    const filePath = getFileName(remark, id)
+    const fullFilePath = path.join(__dirname, SUBS_FOLDER, filePath);
+
+    res.sendFile(fullFilePath, (err) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                res.status(404).send('File not found');
+            } else {
+                res.status(500).send('Server error');
+            }
+        }
+    });
+});
+
+function getFileName(remark, id) {
+    return `${remark}_${id}`
+}
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
